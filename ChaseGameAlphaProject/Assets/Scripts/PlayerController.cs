16d7ac18ea0669b19a83adff666ea67eab193d5c
B00161374 <B00161374@mytudublin.ie>
2024-11-12 12:38:26 +0000

    Player Movement
    Added player movement and controls (Spacebar to jump)

---
+++ ChaseGameAlphaProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * 500, ForceMode.Impulse);
+        }
+        transform.Translate(Vector3.forward * Time.deltaTime * -10);
+    }
+}
