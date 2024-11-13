1e7ec65cea5132b689d727e98cd19bbc47e0b0b6
B00161374 <B00161374@mytudublin.ie>
2024-11-13 22:23:23 +0000

    Game Over
    Added game over text when the chaser touches the player.

--- ChaseGameAlphaProject/Assets/Scripts/Chase.cs
+++ ChaseGameAlphaProject/Assets/Scripts/Chase.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Chase : MonoBehaviour
 {
     public GameObject Sphere;
     public GameObject Player;
     public float speed;
+    public TMP_Text gameOverText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOverText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         Sphere.transform.position = Vector3.MoveTowards(Sphere.transform.position, Player.transform.position, speed);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the Sphere collided with the Player
+        if (collision.gameObject == Player)
+        {
+            // Show the Game Over text
+            gameOverText.gameObject.SetActive(true);
+
+            // Stop the game by setting the time scale to zero
+            Time.timeScale = 0;
+        }
     }
 }
