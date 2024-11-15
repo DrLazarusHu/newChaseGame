83b7422fc74c9144da46cd305dabb5662bc815bb
B00161374 <B00161374@mytudublin.ie>
2024-11-15 13:56:02 +0000

    Finish Text
    Added a Finish text to display when the player completes the level

--- ChaseGameAlphaProject/Assets/Scripts/Finishtext.cs
+++ ChaseGameAlphaProject/Assets/Scripts/Finishtext.cs
@@ -3,37 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-
-public class Finishtext : MonoBehaviour
+public class FinishText : MonoBehaviour
 {
-
-    public GameObject Finish;
-    public GameObject Player;
-    public TextMeshProUGUI winText;
-
+    public GameObject Finish;  // Reference to the Finish object
+    public TextMeshProUGUI winText;  // Reference to the TextMeshPro UI element
 
     void OnCollisionEnter(Collision collision)
-        {
-            // Check if the Sphere collided with the Player
-            if (collision.gameObject == Finish)
-            {
-                // Show the Game Over text
-                winText.gameObject.SetActive(true);
-
-                // Stop the game by setting the time scale to zero
-                Time.timeScale = 0;
-            }
-        }
-
-    // Start is called before the first frame update
-    void Start()
     {
-        
+        // Check if the Player collided with the Finish block
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            winText.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Ensure the text is disabled at the start
+    void Start()
     {
-        
+        winText.gameObject.SetActive(false);
     }
 }
