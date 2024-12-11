170fad3c78c3ca591c60aa36e6ca674d1484a4b7
DrLazarusHu <b00164233@mytudublin.ie>
2024-12-11 22:02:16 +0000

    finishing touches
    added colours and menu button for level 3 and music

---
+++ ChaseGameAlphaProject/Assets/Scripts/MainMenu3.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu3 : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("level 3");
+        
+
+    }
+}
